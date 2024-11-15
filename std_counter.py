9e6bef6410340484e7cf6c757f92893a074863fd
afarkas <afarkas@verticallaboratories.com>
2024-11-15 13:15:45 -0800

    basic file logging; clean up a few unused comments

--- std_counter.py
+++ std_counter.py
@@ -1,5 +1,6 @@
 ### inherited from the pulse_width.py program, this will attempt to count clock pulses intead of determining pulse width 
-# Requires an external clock fed into the input, and the sample wire to be fed into the gate ###
+# Requires an external clock fed into the input, and the sample wire to be fed into the gate, which counts clock pulses 
+# Next step is to add file logging capabilities for events with timestamps ###
 
 from __future__ import absolute_import, division, print_function
 from builtins import *  # @UnusedWildImport
@@ -12,13 +13,19 @@ from mcculw.ul import ULError
 from mcculw.device_info import DaqDeviceInfo
 
 import time
+import datetime as dt
 import os
+import pandas as pd
 
 global max_counter_channels
 global counter_tick_exp
 global counter_tick
 global update_rate
 update_rate = 3   # how fast the terminal refreshes, in seconds
+
+#
+# TODO: break the below into functions when ready to add in GUI elements
+#
 
 if __name__ == "__main__":
     # start by discovering which DAQ are on the USB bus
@@ -59,10 +66,37 @@ if __name__ == "__main__":
 
     #TODO: add the asynchronous or threaded reads here (fun)
     try:
+        # create the full file name and write the header 
+        # TODO: add in time and date stamps to filename
+        # TODO: add in a way to sae to a specific directory
+        filename = 'test.csv'
+        full_filename = os.path.join(os.getcwd(),filename)
+
+        # open the file quickly so that we can write the header
+        try:
+            file = open(full_filename, 'w')
+        except:
+            print('ERROR: file is open already. close file and try again')
+            quit()
+        else:
+            board_names = [(daqs_discovered[b].product_name + ' ' + daqs_discovered[b].unique_id) for b in range(0,len(daqs_discovered))]
+            test_header = f"""Begin logging at {dt.datetime.now()}\n
+            Num. Boards is {len(daqs_discovered)} - {' ID: '.join(board_names[n] for n in range(0,len(board_names)))}\n
+            \n"""
+            file.write(test_header)
+            file.close()
+
+        # start logging using dataframe
+        df = pd.DataFrame(columns=['Time'] + 
+                          ['B'+str(b)+',C'+str(c) for b in range(0,len(daqs_discovered)) for c in range(0,max_counter_channels)])
+        df.to_csv(full_filename, index=False, columns=df.columns, mode='a')
+        
+        # now egin looping for data capture
         while True:
             # give user an out
             print('\nPress Ctrl+C to exit loop')
 
+            data_list = list()  # container for data from channel data
             # loop over number of boards
             for board in range(0,len(daqs_discovered)):
                 # get device info
@@ -73,10 +107,12 @@ if __name__ == "__main__":
                     pulse_width = ul.c_in_32(device_info.board_num,counter_num)*counter_tick
                     # change to microseconds and round (formatting and presentation)
                     pulse_width_us = round(pulse_width/1E-6,1)
+                    data_list.append(pulse_width_us)
 
                     # determine the range of the tick, ie greater than 1, 1:10, 10:100, beyond 100?
                     if pulse_width_us >= 1:
                         if (pulse_width_us >= 1) & (pulse_width_us < 10):
+                            # TODO: add in event logging
                             print('Board ' + str(device_info.board_num) + ', Counter ' + str(counter_num) + ', detected 1 us event' + ' of ' + str(pulse_width_us) + ' us')
                         if (pulse_width_us >= 10) & (pulse_width_us < 100):
                             print('Board ' + str(device_info.board_num) + ', Counter ' + str(counter_num) + ', detected 10 us event' + ' of ' + str(pulse_width_us) + ' us')
@@ -88,20 +124,18 @@ if __name__ == "__main__":
 
                     else:
                         print('Board ' + str(device_info.board_num) + ', Counter ' + str(counter_num) + ', no event detected')
-                # try to c_in_scan to reset the counters
-                # scan_buffer = ul.win_buf_alloc_32(10)
-                # ul.c_in_scan(board,0,max_counter_channels,10,1,scan_buffer,ENUMS.ScanOptions.CONTINUOUS)
-
-
+            
+            # append the data to the df
+            pd.DataFrame([str(pd.to_datetime(dt.datetime.now()))] + data_list).T.to_csv(full_filename, header=False, index=False, mode='a')
             # loop print ~once a second
             time.sleep(update_rate)
+
             # clear the terminal screen for some formatting
             os.system('cls' if os.name == 'nt' else 'clear')
+            # TODO: add in logging of non events every time step as well as events (above)
 
     except KeyboardInterrupt:
         print('EXIT: Keyboard Interrupt')
         pass
-
-#get_daqi_info()
 
 print('wait')
